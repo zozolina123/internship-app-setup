cbc9edee3c65bd9e01cec01240753442c2bc5322
ASDASD Serbanan <deaconu.serban@gmail.com>
2020-11-05 14:04:53 +0200

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,72 @@
 import './App.scss';
 
-import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import IntlWrapper from './lang/IntlWrapper';
-import logo from './logo.svg';
+import ResponsiveNavbar, { drawerWidth } from './components/navigation/ResponsiveNavbar';
+import CustomThemeProvider, { ThemeContext } from './components/wrappers/ThemeWrapper';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        toolbar: theme.mixins.toolbar,
+        content: {
+            flexGrow: 1,
+            padding: theme.spacing(2),
+            [theme.breakpoints.up('md')]: {
+                marginLeft: `${drawerWidth}px`,
+            },
+        },
+    }),
+);
 
 const App: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <IntlWrapper>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        <FormattedMessage
-                            id="text"
-                            values={{
-                                what: 'react-intl',
-                            }}
-                        />
-                    </p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        <FormattedMessage id="other-text" />
-                    </a>
-                </header>
-            </div>
-        </IntlWrapper>
+        <CustomThemeProvider>
+            <Router>
+                <ResponsiveNavbar />
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <Switch>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/settings">
+                            <Settings />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </main>
+            </Router>
+        </CustomThemeProvider>
     );
 };
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Settings(props: any) {
+    return (
+        <div>
+            <h2>Settings</h2>
+            <ThemeContext.Consumer>
+                {({ palleteType, changePalleteType }) => (
+                    <button onClick={() => changePalleteType(palleteType === 'dark' ? 'light' : 'dark')}>
+                        Change theme
+                    </button>
+                )}
+            </ThemeContext.Consumer>
+        </div>
+    );
+}
 
 export default App;
